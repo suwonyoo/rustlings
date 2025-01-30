c51c7f3bf9f80b63874d28f2672e560320977f3e
suwonyoo <qkfkaroql789@g.skku.edu>
2025-01-30 22:23:56 +0900

    Initial commit

---
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // TODO: Create an array called `a` with at least 100 elements in it.
+    // let a = ???
+    // let a : [i32; 2] = [1,2];
+
+    let a = [3; 100];
+    // let a : [i32; 100];
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+        panic!("Array not big enough, more elements needed");
+    }
+}
